9e499820fdf62ea3b18bf81095d0fda385b6b6b9
Ken1567 <garrovillomanuelkennon@gmail.com>
2023-03-10 16:46:17 +0800

    Change to localhost

--- server/app.js
+++ server/app.js
@@ -10,8 +10,8 @@ import multer from "multer";
 
 const pool = connectDatabase();
 const app = express();
-// const PORT = 8000;
-const PORT = process.env.PORT || 8000;
+const PORT = 8000;
+// const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
--- server/pool.js
+++ server/pool.js
@@ -1,27 +1,27 @@
 import pg from "pg";
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
 
-const prodConfig = {
-  connectionString: process.env.DATABASE_URL,
-  ...(process.env.NODE_ENV === "production" && {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  }),
-};
+// const prodConfig = {
+//   connectionString: process.env.DATABASE_URL,
+//   ...(process.env.NODE_ENV === "production" && {
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   }),
+// };
 
-const connectDatabase = () => {
-  const pool = new pg.Pool(prodConfig);
-  return pool;
-};
-// function connectDatabase() {
-//   const pool = new pg.Pool({
-//     user: "postgres",
-//     password: "1234",
-//     database: "Users",
-//     host: "localhost",
-//   });
+// const connectDatabase = () => {
+//   const pool = new pg.Pool(prodConfig);
 //   return pool;
-// }
+// };
+function connectDatabase() {
+  const pool = new pg.Pool({
+    user: "postgres",
+    password: "1234",
+    database: "Users",
+    host: "localhost",
+  });
+  return pool;
+}
 export { connectDatabase };
